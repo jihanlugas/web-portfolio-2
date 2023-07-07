51d14ba691bb7ca919a154dbd8d958390f59d729
jihanlugas <jihanlugas2@gmail.com>
2023-07-07 15:01:49 +0700

    favicon

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,147 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Montserrat } from 'next/font/google';
 
+const montserrat = Montserrat({
+  subsets: ['latin']
+})
 
+const menu = {
+  en: [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+    {
+      name: "Education",
+      path: "/education",
+    },
+    {
+      name: "Experience",
+      path: "/experience",
+    },
+    {
+      name: "Project",
+      path: "/project",
+    },
+    {
+      name: "Skill",
+      path: "/skill",
+    },
+    {
+      name: "Contact",
+      path: "/contact",
+    },
+  ],
+  id: [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+    {
+      name: "Education",
+      path: "/education",
+    },
+    {
+      name: "Experience",
+      path: "/experience",
+    },
+    {
+      name: "Project",
+      path: "/project",
+    },
+    {
+      name: "Skill",
+      path: "/skill",
+    },
+    {
+      name: "Contact",
+      path: "/contact",
+    },
+  ],
+  jp: [
+    {
+      name: "ホーム",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+    {
+      name: "Education",
+      path: "/education",
+    },
+    {
+      name: "Experience",
+      path: "/experience",
+    },
+    {
+      name: "Project",
+      path: "/project",
+    },
+    {
+      name: "Skill",
+      path: "/skill",
+    },
+    {
+      name: "Contact",
+      path: "/contact",
+    },
+  ],
+};
 
 const About = () => {
+
+  const router = useRouter();
+
   return (
     <>
-      <div>About</div>
+      <div>
+        <div className="flex justify-center shadow">
+          <div className='h-20 w-full max-w-4xl flex items-center justify-between px-4'>
+            <div className={`text-3xl font-bold ${montserrat.className}`}>
+              <Link href={'/'}>
+                Jihan Lugas
+              </Link>
+            </div>
+            <div className="hidden md:flex">
+              {menu['en'].map((data, key) => {
+                const isSelected = router.pathname === data.path;
+                return (
+                  <Link key={key} href={data.path}>
+                    <div className={isSelected ? 'text-green-600 mr-4' : 'mr-4 duration-500 hover:text-green-600'} key={key}>{data.name}</div>
+                  </Link>
+                )
+              })}
+            </div>
+            <div className="flex md:hidden">
+              {menu['en'].map((data, key) => {
+                const isSelected = router.pathname === data.path;
+                return (
+                  <Link key={key} href={data.path}>
+                    <div className={isSelected ? 'text-green-600 mr-4' : 'mr-4 duration-500 hover:text-green-600'} key={key}>{data.name}</div>
+                  </Link>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className='w-full max-w-4xl'>
+            <div>asd</div>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { FaLinkedinIn, FaGithub, FaGoogle } from 'react-icons/fa';
 import Typed from 'react-typed';
-import { Montserrat } from 'next/font/google'
+import { Montserrat } from 'next/font/google';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
 
 const montserrat = Montserrat({
   subsets: ['latin']
@@ -124,9 +126,23 @@ const Index = () => {
   const router = useRouter();
   const { locale } = router;
 
+  const [initLoading, setInitLoading] = useState<boolean>(true);
+  const [hideLoading, setHideLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setInitLoading(false)
+
+    // timeout duration sama dengan duration login
+    setTimeout(() => {
+      setHideLoading(!hideLoading)
+    }, 5000);
+  }, [])
+
   return (
     <>
-      <div className={'h-screen flex items-center justify-center bg-gradient-to-r from-cyan-200 to-gray-200'}>
+      <div className={'h-screen flex items-center justify-center'}>
+        <div className={`h-screen w-screen bg-black absolute duration-5000 ${hideLoading ? 'hidden' : !initLoading ? ' opacity-0' : 'opacity-100'}`}>
+        </div>
         <div className={`flex flex-col p-8 md:p-0 w-full md:max-w-xl ${montserrat.className}`}>
           <div className='mb-4 font-bold'>
             <h1>{'Jihan Lugas'}</h1>
@@ -146,15 +162,17 @@ const Index = () => {
               <Typed
                 className='text-green-600 underline underline-offset-8'
                 strings={typed[locale]}
+                // startDelay={5000}
                 typeSpeed={160}
                 loop={true}
               />
             </h2>
           </div>
-          <div className='hidden md:flex mb-8'>
+          {/* <div className='hidden md:flex mb-8'>
             {menu['en'].map((data, key) => {
-              const isSelected = router.pathname.indexOf(data.path) !== -1;
-
+              console.log('router.pathname ', router.pathname)
+              console.log('data.path ', data.path)
+              const isSelected = router.pathname === data.path;
               return (
                 <Link key={key} href={data.path}>
                   <div className={isSelected ? 'text-green-600 mr-4' : 'mr-4 duration-500 hover:text-green-600'} key={key}>{data.name}</div>
@@ -163,7 +181,7 @@ const Index = () => {
               )
             })
             }
-          </div>
+          </div> */}
           <div className="flex mb-4">
             <Link href={'https://www.linkedin.com/in/jihan-lugas-13a914195'} target='_blank'>
               <div className='flex justify-center items-center h-10 w-10 mr-2 bg-gray-600 hover:bg-green-600 text-white rounded-full'>
