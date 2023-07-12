a0b4c34f65ecc2e8d01def51b0c1a4958b1a52b0
jihanlugas <jihanlugas2@gmail.com>
2023-07-12 21:36:26 +0700

    Experiece description

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -28,24 +28,30 @@ const Experience: NextPage<Props> = ({ children }) => {
             </div>
             <div className="grid grid-cols-1 gap-4 p-4">
               <div className='shadow border rounded p-4'>
-                <div className='mb-2'>
-                  <div className='text-lg'>{'Back End Developer'}</div>
+                <div className=''>
+                  <div className='text-xl text-green-600 mb-2'>{'Back End Developer'}</div>
                   <div className=''>{'Lintas Media Danawa | Full-time'}</div>
                 </div>
-                <div className='text-gray-600 mb-4'>
+                <div className='text-gray-600 mb-4 text-sm'>
                   <div>{displayDate(lmdStart, 'MMM YYYY') + ' - ' + ' Present ' + ' · ' + dateDiff(lmdStart, lmdEnd)}</div>
                   <div>{'Depok, West Java, Indonesia | Remote'}</div>
                 </div>
                 <div><strong className='mr-2'>Skills:</strong>{'PostgreSQL · Go (Programming Language)'}</div>
               </div>
               <div className='shadow border rounded p-4'>
-                <div className='mb-2'>
-                  <div className='text-lg'>{'Back End Developer'}</div>
+                <div className=''>
+                  <div className='text-xl text-green-600 mb-2'>{'Back End Developer'}</div>
                   <div className=''>{'PT. Wnw Teknodigie | Full-time'}</div>
                 </div>
-                <div className='text-gray-600 mb-4'>
+                <div className='text-gray-600 mb-4 text-sm'>
                   <div>{displayDate(wnwStart, 'MMM YYYY') + ' - ' + displayDate(wnwEnd, 'MMM YYYY') + ' · ' + dateDiff(wnwStart, wnwEnd)}</div>
                   <div>{'Jakarta, Indonesia'}</div>
+                </div>
+                <div className='mb-4'>
+                  <div className=''>Building up Point Of Sales (POS) system</div>
+                  <div className=''>Building up Property Management System (PMS)</div>
+                  <div className=''>Integrated between PMS and Traveloka</div>
+                  <div className=''>Integrated between PMS and Siteminder</div>
                 </div>
                 <div><strong className='mr-2'>Skills:</strong>{'PostgreSQL · PHP · Next.js · React.js · JavaScript · MySQL · React Native · Go (Programming Language)'}</div>
               </div>
